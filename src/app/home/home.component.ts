af4d173c87ab5883d9c28c573fec3a5b4e397190
Richard Luo <rluo@omniupdate.com>
2019-11-04 17:25:24 -0800

    sm2 responsive done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
-	contactFormModel = <any>{}
+	contactFormModel = <any>{};
 	emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
 	sendBtnText = 'Send Message'
 	constructor() { }
